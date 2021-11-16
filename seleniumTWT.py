133c8cb80b1f6585c575b8848ef9e24e877c1b97
Farhan Hasan <hasanfarhan33@gmail.com>
2021-11-16 22:55:33 +0300

    Automated Cookie Clicker

--- seleniumTWT.py
+++ seleniumTWT.py
@@ -5,6 +5,7 @@ import time
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.action_chains import ActionChains
 
 PATH = "C:\Program Files\geckodriver.exe"
 driver = webdriver.Firefox(executable_path=PATH)
@@ -32,24 +33,47 @@ driver = webdriver.Firefox(executable_path=PATH)
 #     driver.quit()
 
 #VIDEO 3: Page Navigating and clicking elements
-driver.get("https://techwithtim.net")
-link = driver.find_element_by_link_text("Python Programming")
-link.click()
+# driver.get("https://techwithtim.net")
+# link = driver.find_element_by_link_text("Python Programming")
+# link.click()
+#
+# try:
+#     element = WebDriverWait(driver, 10).until(
+#         EC.presence_of_element_located((By.LINK_TEXT, "Beginner Python Tutorials"))
+#     )
+#     element.click()
+#
+#     element = WebDriverWait(driver, 10).until(
+#         EC.presence_of_element_located((By.ID, "sow-button-19310003"))
+#     )
+#     element.click()
+#
+#     driver.back()
+#     driver.back()
+#     driver.back()
+#
+# except:
+#     driver.quit()
 
-try:
-    element = WebDriverWait(driver, 10).until(
-        EC.presence_of_element_located((By.LINK_TEXT, "Beginner Python Tutorials"))
-    )
-    element.click()
+#VIDEO 4: ActionChains & Automating Cookie Clicker
+driver.get("https://orteil.dashnet.org/cookieclicker/")
 
-    element = WebDriverWait(driver, 10).until(
-        EC.presence_of_element_located((By.ID, "sow-button-19310003"))
-    )
-    element.click()
+driver.implicitly_wait(5)
 
-    driver.back()
-    driver.back()
-    driver.back()
+cookie = driver.find_element_by_id("bigCookie")
+cookie_count = driver.find_element_by_id("cookies")
+items = [driver.find_element_by_id("productPrice" + str(i)) for i in range(1,-1,-1)]
 
-except:
-    driver.quit()
+actions = ActionChains(driver)
+actions.click(cookie)
+
+for i in range(5000):
+    actions.perform()
+    count = int(cookie_count.text.split(" ")[0])
+    for item in items:
+        value = int(item.text)
+        if value <= count:
+            upgrade_actions = ActionChains(driver)
+            upgrade_actions.move_to_element(item)
+            upgrade_actions.click()
+            upgrade_actions.perform()
